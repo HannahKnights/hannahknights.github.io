00d5ec5ddf4b0e7a2e4d949c98203dbb3cce3da4
Hannah Knights <hannah.knights@hotmail.co.uk>
2026-08-31 15:56:10 +0100

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,21 +1,30 @@
 
 let words = ["is a work in progress", "is a something", "is an artist", "is loading...", "is interested in culture & technology", "likes words, likes images, likes both", "is interested in the past and the future", "is thinking..."];
 let index = 0;
-let w = innerWidth - 20;
-let h = 100;
 
 function sketch1(p) {
 
     p.setup = function() {
-        let canvas = p.createCanvas(w, h);
+        let container = document.getElementById("canvas");
+        let canvas = p.createCanvas(container.offsetWidth, 100);
         canvas.parent("canvas");
-        p.textSize('1em');
+        p.textSize(getTextSize());
         // Change word every 2000 milliseconds (2 second)
         setInterval(changeWord, 2000);
     }
     p.draw = function() {
         p.clear();
         p.text("Hannah Knights " + words[index], 20, 40);
+    }
+
+    p.windowResized = function() {
+        let container = document.getElementById("canvas");
+        p.resizeCanvas(container.offsetWidth, 100);
+        p.textSize(getTextSize());
+    }
+
+    function getTextSize() {
+        return window.innerWidth < 750 ? 18 : 20;
     }
 
     function changeWord() {
